b85e1687e1d00ad819fb48bef5db1a507a7ecf17
Michael Mol <michael@minter.nl>
2014-03-29 15:58:34 +0100

    Fixes real navigation response. - Changed module_invoke and module_implements, per https://drupal.org/node/1894902 - Changed implementation  coffee_coffee_commands. - Implemented hasPermission

--- lib/Drupal/coffee/Controller/CoffeeController.php
+++ lib/Drupal/coffee/Controller/CoffeeController.php
@@ -8,7 +8,6 @@
 namespace Drupal\coffee\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Drupal\Core\Controller\ControllerBase;
 
 /**
@@ -22,17 +21,77 @@ class CoffeeController extends ControllerBase {
   public function coffeeData() {
     $output = array();
 
-    // @todo Placeholder.
-    for ($i=1; $i<9; $i++){
-      $output[] = array(
-        'label' => 'label'.$i,
-        'value' => 'value'.$i,
-        'command' => 'command',
-      );
+    // Get configured menus from config.
+    $menus = \Drupal::config('coffee.configuration')->get('coffee_menus');
+
+    foreach ($menus as $v) {
+      if ($v === '0') {
+          continue;
+      }
+
+      $menu_tree = \Drupal::service('menu_link.tree');
+
+      $menu = $menu_tree->buildAllData($v);
+
+      foreach ($menu as $link) {
+        $command = $v == 'user-menu' ? ':user' : NULL ;
+        $this->coffee_traverse_below($link, $output, $command);
+      }
+    }
+
+    module_load_include('inc', 'coffee', 'coffee.hooks');
+    $commands = array();
+
+    foreach (\Drupal::moduleHandler()->getImplementations('coffee_commands') as $module) {
+      $commands = array_merge($commands, \Drupal::moduleHandler()->invoke($module, 'coffee_commands', array()));
+    }
+
+    if (!empty($commands)) {
+      $output = array_merge($output, $commands);
     }
 
 
-    return new JsonResponse($output);
+    foreach ($output as $k => $v) {
+      if ($v['value'] == '<front>') {
+        unset($output[$k]);
+        continue;
+      }
+
+      // Filter out XSS.
+      $output[$k]['label'] = filter_xss($output[$k]['label']);
+
+    }
+
+    // Reindex the array.
+    $output = array_values($output);
+
+
+      return new JsonResponse($output);
+  }
+
+  /**
+   * Function coffee_traverse_below().
+   *
+   * Helper function to traverse down through a menu structure.
+   */
+  protected function coffee_traverse_below($link, &$output, $command = NULL) {
+    $l = isset($link['link']) ? $link['link'] : array();
+
+    // Only add if user has access.
+    if (isset($l['access']) && $l['access']) {
+      $label = (!empty($l['title']) ? $l['title'] : $l['link_title']);
+      $output[] = array(
+        'value' => $l['link_path'],
+        'label' => $label,
+        'command' => $command,
+      );
+    }
+
+    if (isset($link['below']) && is_array($link['below'])) {
+      foreach ($link['below'] as $below_link) {
+        $this->coffee_traverse_below($below_link, $output);
+      }
+    }
   }
 
 }
