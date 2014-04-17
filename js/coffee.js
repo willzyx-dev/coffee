0e2f97e92d5ab3ab6c30f1d1e87526094d12b8ef
killua99 <killua99@699418.no-reply.drupal.org>
2014-04-17 17:09:19 +0200

    Issue #2243583 by killua99: Fixing JS CodeStandar.

--- js/coffee.js
+++ js/coffee.js
@@ -3,45 +3,46 @@
  * JavaScript file for the Coffee module.
  */
 
-(function($, Drupal, drupalSettings, DrupalCoffee) {
+(function ($, Drupal, drupalSettings, DrupalCoffee) {
   // Remap the filter functions for autocomplete to recognise the
   // extra value "command".
   var proto = $.ui.autocomplete.prototype,
-  	initSource = proto._initSource;
+    initSource = proto._initSource;
+
   function filter(array, term) {
-  	var matcher = new RegExp( $.ui.autocomplete.escapeRegex(term), 'i');
-  	return $.grep(array, function(value) {
-                return matcher.test(value.command) || matcher.test(value.label) || matcher.test(value.value);
-  	});
+    var matcher = new RegExp($.ui.autocomplete.escapeRegex(term), 'i');
+    return $.grep(array, function (value) {
+      return matcher.test(value.command) || matcher.test(value.label) || matcher.test(value.value);
+    });
   }
 
   $.extend(proto, {
-  	_initSource: function() {
-  		if ($.isArray(this.options.source)) {
-  			this.source = function(request, response) {
-  				response(filter(this.options.source, request.term));
-  			};
-  		}
-  		else {
-  			initSource.call(this);
-  		}
-  	}
+    _initSource: function () {
+      if ($.isArray(this.options.source)) {
+        this.source = function (request, response) {
+          response(filter(this.options.source, request.term));
+        };
+      }
+      else {
+        initSource.call(this);
+      }
+    }
   });
 
   DrupalCoffee = DrupalCoffee || {};
 
   Drupal.behaviors.coffee = {
-    attach: function() {
-      $('body').once('coffee', function() {
+    attach: function () {
+      $('body').once('coffee', function () {
         var body = $(this);
         DrupalCoffee.bg.appendTo(body).hide();
         DrupalCoffee.wrapper.appendTo('body').addClass('hide-form');
         DrupalCoffee.form
-        .append(DrupalCoffee.label)
-        .append(DrupalCoffee.field)
-        .append(DrupalCoffee.results)
-        .wrapInner('<div id="coffee-form-inner" />')
-        .appendTo(DrupalCoffee.wrapper);
+          .append(DrupalCoffee.label)
+          .append(DrupalCoffee.field)
+          .append(DrupalCoffee.results)
+          .wrapInner('<div id="coffee-form-inner" />')
+          .appendTo(DrupalCoffee.wrapper);
 
         // Load autocomplete data set, consider implementing
         // caching with local storage.
@@ -52,17 +53,17 @@
         $.ajax({
           url: drupalSettings.path.basePath + 'admin/coffee/get-data',
           dataType: 'json',
-          success: function(data) {
+          success: function (data) {
             DrupalCoffee.dataset = data;
 
             // Apply autocomplete plugin on show
             var $autocomplete = $(DrupalCoffee.field).autocomplete({
               source: DrupalCoffee.dataset,
-              focus: function( event, ui ) {
+              focus: function (event, ui) {
                 // Prevents replacing the value of the input field
                 event.preventDefault();
               },
-              select: function(event, ui) {
+              select: function (event, ui) {
                 DrupalCoffee.redirect(ui.item.value, event.metaKey);
                 event.preventDefault();
 
@@ -70,54 +71,56 @@
               },
               delay: 0,
               appendTo: DrupalCoffee.results
-           });
-            
-           $autocomplete.data(autocomplete_data_element)._renderItem = function(ul, item) {
+            });
+
+            $autocomplete.data(autocomplete_data_element)._renderItem = function (ul, item) {
               return  $('<li></li>')
-                      .data('item.autocomplete', item)
-                      .append('<a>' + item.label + '<small class="description">' + item.value + '</small></a>')
-                      .appendTo(ul);
+                .data('item.autocomplete', item)
+                .append('<a>' + item.label + '<small class="description">' + item.value + '</small></a>')
+                .appendTo(ul);
             };
 
             // We want to limit the number of results.
-            $(DrupalCoffee.field).data(autocomplete_data_element)._renderMenu = function(ul, items) {
-          		var self = this;
-          		items = items.slice(0, 7); // @todo: max should be in Drupal.settings var.
-          		$.each( items, function(index, item) {
-          			self._renderItemData(ul, item);
-          		});
-          	};
+            $(DrupalCoffee.field).data(autocomplete_data_element)._renderMenu = function (ul, items) {
+              var self = this;
+              items = items.slice(0, 7); // @todo: max should be in Drupal.settings var.
+              $.each(items, function (index, item) {
+                self._renderItemData(ul, item);
+              });
+            };
 
-          	// On submit of the form select the first result if available.
-          	DrupalCoffee.form.submit(function() {
-          	  var firstItem = jQuery(DrupalCoffee.results).find('li:first').data('item.autocomplete');
-          	  if (typeof firstItem == 'object') {
-          	    DrupalCoffee.redirect(firstItem.value, false);
-          	  }
+            // On submit of the form select the first result if available.
+            DrupalCoffee.form.submit(function () {
+              var firstItem = jQuery(DrupalCoffee.results).find('li:first').data('item.autocomplete');
+              if (typeof firstItem == 'object') {
+                DrupalCoffee.redirect(firstItem.value, false);
+              }
 
-          	  return false;
-          	});
+              return false;
+            });
           },
-          error: function() {
+          error: function () {
             DrupalCoffee.field.val('Could not load data, please refresh the page');
           }
         });
 
         // Key events
-        $(document).keydown(function(event) {
+        $(document).keydown(function (event) {
 
           // Show the form with alt + D. Use 2 keycodes as 'D' can be uppercase or lowercase.
           if (DrupalCoffee.wrapper.hasClass('hide-form') &&
-        		  event.altKey === true && 
-        		  // 68/206 = d/D, 75 = k. 
-        		  (event.keyCode === 68 || event.keyCode === 206  || event.keyCode === 75)) {
+            event.altKey === true &&
+              // 68/206 = d/D, 75 = k.
+            (event.keyCode === 68 || event.keyCode === 206 || event.keyCode === 75)) {
             DrupalCoffee.coffee_show();
             event.preventDefault();
           }
           // Close the form with esc or alt + D.
-          else if (!DrupalCoffee.wrapper.hasClass('hide-form') && (event.keyCode === 27 || (event.altKey === true && (event.keyCode === 68 || event.keyCode === 206)))) {
-            DrupalCoffee.coffee_close();
-            event.preventDefault();
+          else {
+            if (!DrupalCoffee.wrapper.hasClass('hide-form') && (event.keyCode === 27 || (event.altKey === true && (event.keyCode === 68 || event.keyCode === 206)))) {
+              DrupalCoffee.coffee_close();
+              event.preventDefault();
+            }
           }
         });
       });
@@ -130,7 +133,7 @@
   /**
    * Open the form and focus on the search field.
    */
-  DrupalCoffee.coffee_show = function() {
+  DrupalCoffee.coffee_show = function () {
     DrupalCoffee.wrapper.removeClass('hide-form');
     DrupalCoffee.bg.show();
     DrupalCoffee.field.focus();
@@ -140,7 +143,7 @@
   /**
    * Close the form and destroy all data.
    */
-  DrupalCoffee.coffee_close = function() {
+  DrupalCoffee.coffee_close = function () {
     DrupalCoffee.field.val('');
     //DrupalCoffee.results.empty();
     DrupalCoffee.wrapper.addClass('hide-form');
@@ -151,7 +154,7 @@
   /**
    * Close the Coffee form and redirect.
    */
-  DrupalCoffee.redirect = function(path, openInNewWindow) {
+  DrupalCoffee.redirect = function (path, openInNewWindow) {
     DrupalCoffee.coffee_close();
 
     if (openInNewWindow) {
@@ -169,7 +172,7 @@
   DrupalCoffee.results = $('<div id="coffee-results" />');
   DrupalCoffee.wrapper = $('<div class="coffee-form-wrapper" />');
   DrupalCoffee.form = $('<form id="coffee-form" action="#" />');
-  DrupalCoffee.bg = $('<div id="coffee-bg" />').click(function() {
+  DrupalCoffee.bg = $('<div id="coffee-bg" />').click(function () {
     DrupalCoffee.coffee_close();
   });
 
