e26d90e81141cedce2c9d170072417f074f257cb
tbfisher <tbfisher@487090.no-reply.drupal.org>
2015-04-20 10:12:17 +0200

    Issue #2431057 by tbfisher, Jaesin, michaelmol: install error implement Drupal\Core\Form\ConfigFormBaseTrait::getEditableConfigNames()

--- src/Form/CoffeeConfigurationForm.php
+++ src/Form/CoffeeConfigurationForm.php
@@ -30,6 +30,21 @@ class CoffeeConfigurationForm extends ConfigFormBase {
   }
 
   /**
+   * Implements Drupal\Core\Form\ConfigFormBaseTrait::getEditableConfigNames()
+   *
+   * Gets the configuration names that will be editable.
+   *
+   * @return array
+   *   An array of configuration object names that are editable if called in
+   *   conjunction with the trait's config() method.
+   */
+  protected function getEditableConfigNames() {
+    return array(
+      'coffee.configuration',
+    );
+  }
+
+  /**
    * Implements \Drupal\Core\Form\FormInterface::buildForm().
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
