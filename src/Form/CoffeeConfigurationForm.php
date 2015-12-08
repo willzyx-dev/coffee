f2f40c65639b34ab471ef5b6b4af91d457460530
willzyx <willzyxxx@gmail.com>
2015-12-08 14:32:02 +0100

    Add coffee.schema.yml and coffee.configuration.yml

--- src/Form/CoffeeConfigurationForm.php
+++ src/Form/CoffeeConfigurationForm.php
@@ -7,15 +7,8 @@
 
 namespace Drupal\coffee\Form;
 
-use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
-
-use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Cache\CacheBackendInterface;
-use Drupal\Component\Utility\String;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-
 
 /**
  * Configure Coffee for this site.
@@ -25,27 +18,21 @@ class CoffeeConfigurationForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public function getFormID() {
+  public function getFormId() {
     return 'coffee_configuration_form';
   }
 
   /**
-   * Implements Drupal\Core\Form\ConfigFormBaseTrait::getEditableConfigNames()
-   *
-   * Gets the configuration names that will be editable.
-   *
-   * @return array
-   *   An array of configuration object names that are editable if called in
-   *   conjunction with the trait's config() method.
+   * {@inheritdoc}
    */
   protected function getEditableConfigNames() {
-    return array(
+    return [
       'coffee.configuration',
-    );
+    ];
   }
 
   /**
-   * Implements \Drupal\Core\Form\FormInterface::buildForm().
+   * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('coffee.configuration');
@@ -53,29 +40,29 @@ class CoffeeConfigurationForm extends ConfigFormBase {
     $menus = menu_ui_get_menus();
 
     if (!empty($menus)) {
-      // Settings for coffee.
-      $form['coffee_menus'] = array(
+      $form['coffee_menus'] = [
         '#type' => 'checkboxes',
-        '#title' => t('Menus to include'),
-        '#description' => t('Select the menus that should be used by Coffee to search.'),
+        '#title' => $this->t('Menus to include'),
+        '#description' => $this->t('Select the menus that should be used by Coffee to search.'),
         '#options' => $menus,
         '#required' => TRUE,
-        '#default_value' => (array) $config->get('coffee_menus'),
-      );
+        '#default_value' => $config->get('coffee_menus'),
+      ];
     }
+
     return parent::buildForm($form, $form_state);
   }
 
   /**
-   * Implements \Drupal\Core\Form\FormInterface::submitForm().
+   * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    \Drupal::configFactory()->getEditable('coffee.configuration')
-    ->set('coffee_menus', $form_state->getValue('coffee_menus'))
-    ->save();
+    $values = $form_state->getValues();
+    $this->config('coffee.configuration')
+      ->set('coffee_menus', array_filter($values['coffee_menus']))
+      ->save();
 
     parent::submitForm($form, $form_state);
-    // @todo Implement Cache::invalidateTags().
   }
 
 }
---
+++ src/Tests/CoffeeTest.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\coffee\Tests\CoffeeTest.
+ */
+
+namespace Drupal\coffee\Tests;
+
+use Drupal\simpletest\WebTestBase;
+
+/**
+ * Tests Coffee module functionality.
+ *
+ * @group coffee
+ */
+class CoffeeTest extends WebTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = ['coffee', 'menu_ui'];
+
+  /**
+   * The user for tests.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  protected $webUser;
+
+  /**
+   * The user for tests.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  protected $coffeeUser;
+
+  /**
+   * The user for tests.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  protected $coffeeAdmin;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp(){
+    parent::setUp();
+
+    $this->webUser = $this->drupalCreateUser();
+    $this->coffeeUser = $this->drupalCreateUser(['access coffee']);
+    $this->coffeeAdmin = $this->drupalCreateUser(['administer coffee']);
+  }
+
+  /**
+   * Tests coffee configuration form.
+   */
+  public function testCoffeeConfiguration() {
+    $this->drupalGet('admin/config/user-interface/coffee');
+    $this->assertResponse(403);
+
+    $this->drupalLogin($this->coffeeAdmin);
+    $this->drupalGet('admin/config/user-interface/coffee');
+    $this->assertResponse(200);
+    $this->assertFieldChecked('edit-coffee-menus-admin', 'The admin menu is enabled by default');
+
+    $edit = [
+      'coffee_menus[tools]' => 'tools',
+      'coffee_menus[account]' => 'account'
+    ];
+    $this->drupalPostForm('admin/config/user-interface/coffee', $edit, t('Save configuration'));
+    $this->assertText(t('The configuration options have been saved.'));
+
+    $expected = [
+      'admin' => 'admin',
+      'tools' => 'tools',
+      'account' => 'account'
+    ];
+    $config = \Drupal::config('coffee.configuration')->get('coffee_menus');
+    $this->assertEqual($expected, $config, 'The configuration options have been properly saved');
+  }
+
+}
